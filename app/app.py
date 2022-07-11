3ae748c719e543bcb4b1e57943fd6bc8f07448b0
Eric OC <eric@ericoc.com>
2022-07-11 15:37:33 +0000

    image logo, thoughts doc, settings to injects, major clean up

--- app/app.py
+++ app/app.py
@@ -8,8 +8,6 @@ import os
 
 # Run
 app = Flask(__name__)
-if __name__ == '__main__':
-    app.run(debug=True)
 
 # Static content
 @app.route('/favicon.ico')
@@ -34,83 +32,111 @@ def service_unavailable(message):
     return error(message, 503)
 
 def error(message='Sorry! There was an error. Please try again or come back later.', code=500):
-    r = make_response(render_template('seizures.html.j2',
-            timespans=settings.timespans,
-            error_message=message,
-            today=today()),
-        code)
-    return r
+    return make_response(
+                render_template('seizures.html.j2',
+                    error_message   = message
+                ),
+                code
+            )
 
 # Create a function to get today's date in YYYY-MM-DD format in my time zone
 def today():
+
     try:
-        tz = pytz.timezone(settings.timezone)
-        dt = datetime.datetime.today().astimezone(tz).strftime('%Y-%m-%d')
+        tz  = pytz.timezone(settings.timezone)
+        dt  = datetime.datetime.today().astimezone(tz).strftime('%Y-%m-%d')
         return dt
+
     except Exception as e:
         print(f"today:\n{e}")
         return None
 
+
+# Create context processor to make today's date and timespans available to Jinja
+@app.context_processor
+def injects():
+    return dict(
+                today       = today(),
+                timespans   = settings.timespans
+            )
+
+
 # Create a template filter function for Jinja2 to convert ISO date to human-readable in my timezone
 @app.template_filter()
 def get_dow(date=None):
+
     if date and len(date) == 10:
+
         try:
-            dt = datetime.date.fromisoformat(date)
-            date_url = url_for('view_date', date=date)
-            format = f"<a href=\"{date_url}\">%a, %b %d, %Y</a>"
+            dt          = datetime.date.fromisoformat(date)
+            date_url    = url_for('view_date', date=date)
+            format      = f"<a href=\"{date_url}\">%a, %b %d, %Y</a>"
             return dt.strftime(format)
+
         except Exception as e:
             print(f"get_dow: {date}\n{e}")
             return time
+
     else:
         return False
+
 
 # Create a template filter function for Jinja2 to convert InfluxDB timestamps to human-readable in my timezone
 @app.template_filter()
 def format_datetime(time=None):
+
     if time:
+
         try:
-            tz = pytz.timezone(settings.timezone)
-            stime = str(time)
-            unix_time = stime[0:10]
-            ftime = datetime.datetime.fromtimestamp(int(unix_time)).astimezone(tz)
-            date_url = url_for('view_date', date=ftime.strftime('%Y-%m-%d'))
-            event_url = url_for('view_event', event=time)
-            format = f"<a href=\"{date_url}#{unix_time}\">%a, %b %d, %Y</a> @ <a href=\"{event_url}\">%I:%M:%S %p</a>"
+            tz          = pytz.timezone(settings.timezone)
+            stime       = str(time)
+            unix_time   = stime[0:10]
+            ftime       = datetime.datetime.fromtimestamp(int(unix_time)).astimezone(tz)
+            date_url    = url_for('view_date', date=ftime.strftime('%Y-%m-%d'))
+            event_url   = url_for('view_event', event=time)
+            format      = f"<a href=\"{date_url}#{unix_time}\">%a, %b %d, %Y</a> @ <a href=\"{event_url}\">%I:%M:%S %p</a>"
             return ftime.strftime(format)
+
         except Exception as e:
             print(f"format_datetime: {time}\n{e}")
             return time
     else:
         return False
 
+
 # Create a function to connect to InfluxDB
-def dbc(username, password):
+def dbc(username=settings.influxdb['username'], password=settings.influxdb['password']):
+
     try:
-        client = influxdb.InfluxDBClient(
-            host='127.0.0.1',
-            port=8086,
-            username=username,
-            password=password,
-            ssl=False,
-            verify_ssl=False
-        )
-        return client
+        return influxdb.InfluxDBClient(
+                host        = '127.0.0.1',
+                port        = 8086,
+                username    = username,
+                password    = password,
+                ssl         = False,
+                verify_ssl  = False
+            )
+
     except Exception as e:
         print(f"connecting: {client}\n{e}")
         return e
 
+
 # Create a function to clean up the JSON URL-encoded strings, with backslashed spaces for InfluxDB
 def clean_name(name=None):
+
     if name:
+
         try:
             return urllib.parse.unquote(name).replace(u'\xa0', u' ').replace(u"’", u"'").replace("\n", ', ')
+
         except Exception as e:
             print(f"clean_name: {name}\n{e}")
             return name
+
     else:
         return False
+
 
 # Create a function to parse the JSON that we received from add()
 def parse(data):
@@ -139,51 +165,57 @@ def parse(data):
     # Return the line protocol style string of fields
     return fields
 
+
 # Handle requests for specific events ("permalinks")
 @app.route('/event/<int:event>', methods=['GET'])
 def view_event(event=None):
+
     try:
         if event and len(str(event)) == 19:
-            query_where = f"time = {event}"
-            return index(query_where=query_where)
+            return index(query_where = f"time = {event}")
         else:
             return error(message='Sorry, but that is not a valid event! Please try again later, or perform another search.', code=404)
     except Exception as e:
         print(f"view_event:\nevent: {event}\n{e}")
         return error(message='Sorry, but there was an error processing that event! Please try again.', code=500)
 
+
 # Handle time-span requests
 @app.route('/span/<string:span>', methods=['GET'])
 def view_span(span=None):
     try:
         if span in settings.timespans:
-            query_where = f"time > NOW() - {span}"
-            return index(query_where, span=span)
+            return index(
+                        query_where = f"time > NOW() - {span}",
+                        span        = span
+                    )
         else:
             return error(message='Sorry, but that is not a valid time-span! Please try again.', code=501)
     except Exception as e:
         print(f"view_span:\nspan: {span}\n{e}")
         return error(message='Sorry, but there was an error processing that time-span! Please try again.', code=500)
 
+
 # Handle requests for a specific date
 @app.route('/date/<string:date>', methods=['GET'])
 def view_date(date=None):
     try:
         # Find the start and end dates
-        start = datetime.date.fromisoformat(date)
-        end = start + datetime.timedelta(days=1)
+        start   = datetime.date.fromisoformat(date)
+        end     = start + datetime.timedelta(days=1)
 
         # Find the offset of my timezone and add a colon separator for InfluxDB
-        offset = datetime.datetime.now(pytz.timezone(settings.timezone)).strftime('%z')
-        offset_adj = offset[0:3] + ':' + offset[3:6]
+        offset      = datetime.datetime.now(pytz.timezone(settings.timezone)).strftime('%z')
+        offset_adj  = offset[0:3] + ':' + offset[3:6]
 
         # Format the start and end dates into strings for InfluxDB querying, with the appropriate offset
-        format = f"%Y-%m-%dT%H:%M:%S.%f{offset_adj}"
-        qstart = start.strftime(format)
-        qend = end.strftime(format)
-        query_where = f"time > '{qstart}' AND time < '{qend}'"
-
-        return index(query_where, date=start.isoformat())
+        format  = f"%Y-%m-%dT%H:%M:%S.%f{offset_adj}"
+        qstart  = start.strftime(format)
+        qend    = end.strftime(format)
+        return index(
+                    query_where = f"time > '{qstart}' AND time < '{qend}'",
+                    date        = start.isoformat()
+                )
 
     except Exception as e:
         print(f"view_date:\ndate: {date}\n{e}")
@@ -193,26 +225,36 @@ def view_date(date=None):
 @app.route('/', methods=['GET'])
 def index(query_where=None, date=None, span=None):
 
-    # Set a default action, and always sort
+    # Set a default action using the default time-span
     if not query_where:
-        return redirect(url_for('view_span', span=settings.default_timespan))
+        return redirect(
+                    url_for('view_date',
+                        date = today()
+                    )
+                )
 
     # Build the query and always sort
     query = f"SELECT * FROM \"{settings.influxdb['measurement']}\" WHERE {query_where} ORDER BY time DESC"
 
     # Connect to InfluxDB
     try:
-        client = dbc(settings.influxdb['username'], settings.influxdb['password'])
+        client = dbc()
     except:
         return error(message='Sorry! Unfortunately, the database is offline. Please try again later.', code=503)
 
     # Query InfluxDB, get results, and disconnect
     try:
-        results = client.query(query, database=settings.influxdb['database'], epoch='ns')
-        points = results.get_points(measurement=settings.influxdb['measurement'])
+        results = client.query(
+                        query,
+                        database=settings.influxdb['database'],
+                        epoch='ns'
+                    )
+        points  = results.get_points(measurement=settings.influxdb['measurement'])
+
     except Exception as e:
         print(f"querying:\n{e}")
         return error('Sorry! Unfortunately, your query failed. Please try again later, or perform another search.', 500)
+
     finally:
         client = None
 
@@ -220,8 +262,8 @@ def index(query_where=None, date=None, span=None):
     try:
 
         # Convert results to a list and count them
-        list_points = list(points)
-        points_count = len(list_points)
+        list_points     = list(points)
+        points_count    = len(list_points)
 
         # Change HTTP response code if there are no results
         if points_count > 0:
@@ -230,21 +272,23 @@ def index(query_where=None, date=None, span=None):
             code = 404
 
         # Return Jinja2 template and HTTP header with the result count
-        r = make_response(render_template('seizures.html.j2',
-                points=list_points,
-                timespans=settings.timespans,
-                date=date,
-                today=today(),
-                span=span,
-                googlemaps_api_key=settings.googlemaps_api_key,
-                start=settings.start),
-            code)
+        r = make_response(
+                render_template('seizures.html.j2',
+                    points              = list_points,
+                    date                = date,
+                    span                = span,
+                    googlemaps_api_key  = settings.googlemaps_api_key
+                ),
+                code
+            )
+
         r.headers.set('X-Result-Count:', str(points_count))
         return r
 
     except Exception as e:
         print(f"index:\n{e}")
         return error(message='Sorry! Unfortunately the results could not be returned. Please try another search.', code=500)
+
 
 # Create a function to insert to InfluxDB
 @app.route('/add', methods=['POST'])
@@ -284,12 +328,14 @@ def add():
 
     # Connect to InfluxDB, insert, and disconnect
     try:
-        client = dbc(settings.influxdb['username'], settings.influxdb['password'])
-        write_data = f"{settings.influxdb['measurement']},device=\"{device}\",network=\"{network}\" {fields}"
+        client      = dbc()
+        write_data  = f"{settings.influxdb['measurement']},device=\"{device}\",network=\"{network}\" {fields}"
         if client.write(write_data, params={'db': settings.influxdb['database']}, protocol='line'):
             return Response(response='OK', status=201)
+
     except Exception as e:
         print(f"writing:\n{write_data}\n{client}\n{e}")
         return error(message='Sorry! Unfortunately, the database insertion failed.', code=500)
+
     finally:
         client = None
---
+++ app/thoughts.md
@@ -0,0 +1,10 @@
+### Thoughts/Ideas
+
+- Should the marker icons be different per device?
+    * For example, separate computer/phone/watch/default icons
+        - The background of the icons might need to be opaque - or have rounded corners, like the default red ones
+
+- Possibly number the markers on the map (`label: {{ loop.index }}` on the marker)
+
+- Possibly remove labels of places on the map or see how we can have a cleaner map
+    * Maybe even switch try out [OpenStreetMap](https://www.openstreetmap.org/)?
