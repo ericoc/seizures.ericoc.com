9669fb2e3c3f393a9f79e3d067e5e397a0dc4ba0
Eric O'Callaghan <eric@ericoc.com>
2023-08-15 20:57:47 -0400

    rm unused function

--- seizures/views/util.py
+++ seizures/views/util.py
@@ -1,28 +1,16 @@
-import urllib
 from django.core.serializers import serialize
+
 from settings import DEVICE_ICONS
 
 
 def get_ip_address(request):
     """
-    Get user IP address from HTTP request.
+    Get user IP address from request.
     """
     user_ip = request.META.get('HTTP_X_FORWARDED_FOR')
     if user_ip:
         return user_ip.split(',')[0]
     return request.META.get('REMOTE_ADDR')
-
-
-def parse_field(value=None):
-    """
-    Parse URL-encoded string values from Apple shortcut, for database insert.
-    """
-    if value is not None and isinstance(value, str):
-        return urllib.parse.unquote(value) \
-            .replace(u'\xa0', u' ') \
-            .replace(u"’", u"'") \
-            .replace("\n", ', ')
-    return None
 
 
 def seize_context(context=None):
