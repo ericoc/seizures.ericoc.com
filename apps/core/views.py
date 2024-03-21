50c32156fdcf1f37d7f09c7d95ce9039aa07b5a7
Eric O'Callaghan <eric@ericoc.com>
2024-03-21 19:33:00 -0400

    comments and update context/initial

--- apps/core/views.py
+++ apps/core/views.py
@@ -9,9 +9,11 @@ class SeizuresErrorView(SeizuresView):
     status_code = 500
 
     def dispatch(self, request, *args, **kwargs):
+        """Send error message in response."""
         messages.error(request=request, message=self.message)
         return super().dispatch(request, *args, **kwargs)
 
     def render_to_response(self, context, **response_kwargs):
+        """Set status code of the HTTP response."""
         response_kwargs["status"] = self.status_code
         return super().render_to_response(context, **response_kwargs)
--- apps/seizures/views.py
+++ apps/seizures/views.py
@@ -10,17 +10,16 @@ from .models import Seizure
 
 class SeizuresView(PermissionRequiredMixin, FormView):
     """View seizures between start and end times."""
-    dates = initial = {}
+    dates = initial = {"start": None, "end": None}
     form_class = SeizuresSearchDateForm
     http_method_names = ("get", "post")
     permission_required = "seizures.view_seizure"
-    seizures = None
     template_name = "seizures.html"
 
     def setup(self, request, *args, **kwargs):
         """Set default search start and end datetime values."""
-        self.dates["end"] = localtime()
-        self.dates["start"] = self.dates["end"] - settings.DEFAULT_SINCE
+        now = localtime()
+        self.dates = {"start": now - settings.DEFAULT_SINCE, "end": now}
         return super().setup(request, *args, **kwargs)
 
     def form_valid(self, form):
@@ -29,23 +28,23 @@ class SeizuresView(PermissionRequiredMixin, FormView):
         return self.render_to_response(self.get_context_data(form=form))
 
     def get_context_data(self, **kwargs):
-        """Include icons, search datetimes, and seizures JSON in context."""
+        """Include device icons, dates, and seizures JSON in context."""
         context = super().get_context_data(**kwargs)
         context["device_icons"] = settings.DEVICE_ICONS
-        context["start"] = self.dates["start"]
-        context["end"] = self.dates["end"]
-        context["seizures"] = serialize(
-            format="json",
-            queryset=Seizure.objects.filter(
-                timestamp__gte=self.dates["start"],
-                timestamp__lte=self.dates["end"]
-            ).all()
-        )
+
+        for date in ("start", "end"):
+            context[date] = self.dates[date]
+
+        seizures = Seizure.objects.filter(
+            timestamp__gte=self.dates["start"], timestamp__lte=self.dates["end"]
+        ).all()
+        context["seizures"] = serialize(format="json", queryset=seizures)
+
         return context
 
     def get_initial(self):
-        """Fill in search form date inputs with correctly formatted strings."""
+        """Set initial form search date string values."""
         initial = super().get_initial()
         for date in ("start", "end"):
-            initial[date] = initial[date].strftime("%Y-%m-%dT%H:%M:%S")
+            initial[date] = self.dates[date].strftime("%Y-%m-%dT%H:%M:%S")
         return initial
