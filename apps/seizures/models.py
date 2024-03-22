9d64186b713e30dfac293314b273038ea0d07288
Eric O'Callaghan <eric@ericoc.com>
2024-03-21 20:46:16 -0400

    min/max _value in DecimalField should be Decimal type

--- apps/seizures/models.py
+++ apps/seizures/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 from django.core.validators import (
     DecimalValidator, MaxLengthValidator, MaxValueValidator, MinValueValidator
@@ -54,8 +56,8 @@ class Seizure(models.Model):
         help_text="GPS latitude where the seizure was recorded.",
         validators=(
             DecimalValidator(max_digits=20, decimal_places=15),
-            MaxValueValidator(limit_value=90.0),
-            MinValueValidator(limit_value=-90.0)
+            MaxValueValidator(limit_value=Decimal(90.0)),
+            MinValueValidator(limit_value=Decimal(-90.0))
         ),
         verbose_name="GPS Latitude"
     )
@@ -65,8 +67,8 @@ class Seizure(models.Model):
         help_text="GPS longitude where the seizure was recorded.",
         validators=(
             DecimalValidator(max_digits=20, decimal_places=15),
-            MaxValueValidator(limit_value=180.0),
-            MinValueValidator(limit_value=-180.0)
+            MaxValueValidator(limit_value=Decimal(180.0)),
+            MinValueValidator(limit_value=Decimal(-180.0))
         ),
         verbose_name="GPS Longitude"
     )
@@ -84,8 +86,8 @@ class Seizure(models.Model):
         help_text="Device battery (between 1 and 100) upon seizure recording.",
         validators=(
             DecimalValidator(max_digits=20, decimal_places=15),
-            MaxValueValidator(limit_value=100.0),
-            MinValueValidator(limit_value=1.0)
+            MaxValueValidator(limit_value=Decimal(100.0)),
+            MinValueValidator(limit_value=Decimal(1.0))
         ),
         verbose_name="Device Battery"
     )
@@ -97,8 +99,8 @@ class Seizure(models.Model):
         help_text="Device brightness (between 0 and 1) upon seizure recording.",
         validators=(
             DecimalValidator(max_digits=20, decimal_places=15),
-            MaxValueValidator(limit_value=1.0),
-            MinValueValidator(limit_value=0.0)
+            MaxValueValidator(limit_value=Decimal(1.0)),
+            MinValueValidator(limit_value=Decimal(0.0))
         ),
         verbose_name="Device Brightness"
     )
@@ -110,8 +112,8 @@ class Seizure(models.Model):
         help_text="Device volume (between 0 and 1) upon seizure recording.",
         validators=(
             DecimalValidator(max_digits=20, decimal_places=15),
-            MaxValueValidator(limit_value=1.0),
-            MinValueValidator(limit_value=0.0)
+            MaxValueValidator(limit_value=Decimal(1.0)),
+            MinValueValidator(limit_value=Decimal(0.0))
         ),
         verbose_name="Volume"
     )
