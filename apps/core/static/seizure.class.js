f9984df23c9bc4c279e3e9cf08307a28b99990d6
Eric O'Callaghan <eric@ericoc.com>
2024-06-11 16:16:51 -0400

    fix js warnings, clear unnecessary semicolons

--- apps/core/static/seizure.class.js
+++ apps/core/static/seizure.class.js
@@ -5,7 +5,7 @@ const Seizure = class {
     constructor(seizure) {
 
         // Parse seizure fields.
-        this.date = new Date(seizure.pk);
+        this.date = new Date(seizure.pk)
         this.titleDate = this.date.toLocaleTimeString("en-us", {
             weekday: "short",
             year: "numeric",
@@ -33,34 +33,34 @@ const Seizure = class {
             this.address = seizure.fields.address.replace(/\n/g, ", ");
             this.addressText = `<span data-bs-toggle="tooltip" data-bs-title="${this.address}">${this.address}</span>`;
             this.addressLinkText = `<a href="${gmapsURL}${seizure.fields.latitude},${seizure.fields.longitude}" target="_blank" title="Google Maps: ${this.coordinates}">${this.addressText}</a>`;
-        };
+        }
 
         this.altitude = null;
         if (seizure.fields.altitude) {
             this.altitude = parseFloat(seizure.fields.altitude).toFixed(2);
-        };
+        }
 
         this.battery = null;
         if (seizure.fields.battery) {
             this.battery = parseFloat(seizure.fields.battery).toFixed(2);
-        };
+        }
 
         this.brightness = null;
         if (seizure.fields.brightness) {
-            this.brightness = parseFloat(seizure.fields.brightness * 100).toFixed(2);
-        };
+            this.brightness = parseFloat((seizure.fields.brightness * 100).toString()).toFixed(2);
+        }
 
         this.volume = null;
         if (seizure.fields.volume) {
-            this.volume = parseFloat(seizure.fields.volume * 100).toFixed(2);
-        };
+            this.volume = parseFloat((seizure.fields.volume * 100).toString()).toFixed(2);
+        }
 
         this.ssid = null;
         this.ssidText = null;
         if (seizure.fields.ssid) {
             this.ssid = seizure.fields.ssid;
             this.ssidText = `<span data-bs-toggle="tooltip" data-bs-title="${this.ssid}">${this.ssid}</span>`;
-        };
+        }
 
     };
 };
