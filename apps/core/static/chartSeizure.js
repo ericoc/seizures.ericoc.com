b1b5c44250327ef37006dd172b732581be57ac04
Eric O'Callaghan <eric@ericoc.com>
2025-01-20 20:52:32 -0500

    lots of dupe code WIP

---
+++ apps/core/static/chartSeizure.js
@@ -0,0 +1,25 @@
+/* Parse, display, and chart a single seizure. */
+function chartSeizure(seizureData) {
+
+    // List device type icon, and link time.
+    const seizure = new Seizure(seizureData);
+    const listNode = document.createElement("li");
+    listNode.classList.add("list-group-item");
+    listNode.classList.add("list-group-item-action");
+    listNode.classList.add("list-group-item-text");
+    listNode.classList.add("small");
+    listNode.classList.add("card-text");
+    listNode.onclick = function() {
+        window.location.href = `#${seizure.unixTime}`;
+    };
+
+    const linkNode = document.createElement("a");
+    linkNode.title = `${seizure.deviceIcon} ${seizure.titleDate}`;
+    linkNode.href = `#${seizure.unixTime}`;
+    linkNode.appendChild(document.createTextNode(seizure.titleDate));
+    listNode.appendChild(document.createTextNode(seizure.deviceIcon));
+    listNode.appendChild(linkNode);
+    seizureList.appendChild(listNode);
+
+    return seizure;
+};
---
+++ apps/core/static/chartSeizures.js
@@ -0,0 +1,27 @@
+/* Loop through each seizure, displaying them. */
+async function chartSeizures(seizures) {
+
+    // Loop through seizures, displaying each.
+    for (const seizureData of seizures) {
+        await chartSeizure(seizureData);
+    };
+
+    // Append the list of seizure links to the navigation card.
+    seizuresCard.appendChild(seizureList);
+
+    // Add a card footer of the seizure count.
+    const seizureCount = seizures.length;
+    const cardFooter = document.createElement("h5");
+    cardFooter.classList.add("card-footer");
+    cardFooter.classList.add("text-secondary");
+    cardFooter.classList.add("text-center");
+    let countText = `${seizureCount.toLocaleString("en-US")} seizure`;
+    if (seizureCount > 1) { countText += "s"; };
+    document.title += ` ${countText}`;
+    cardFooter.appendChild(document.createTextNode(countText));
+    cardFooter.title = countText;
+    seizuresCard.appendChild(cardFooter);
+
+    // Log if referenced by URL anchor.
+    const anchor = String(window.location.hash).split('#')[1];
+};
