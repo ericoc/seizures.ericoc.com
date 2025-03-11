b57ebfc341dd8eefe4b0f184e391177315d73368
Eric O'Callaghan <eric@ericoc.com>
2025-03-10 20:30:51 -0400

    api filtering all fields

--- apps/seizures/api.py
+++ apps/seizures/api.py
@@ -15,16 +15,11 @@ class SeizureSerializer(ModelSerializer):
 
 class APISeizuresViewSet(ModelViewSet):
     """Django-Rest-Framework (DRF) ModelViewSet API endpoint for seizures."""
-    filterset_fields = fields = "__all__"
+    filter_backends = (OrderingFilter,)
+    filterset_fields = fields = ordering_fields = "__all__"
     model = Seizure
     queryset = model.objects.all()
     serializer_class = SeizureSerializer
-
-    filter_backends = (OrderingFilter,)
-    ordering_fields = (
-        "pk", "timestamp", "device_name", "device_type", "ssid", "brightness",
-        "altitude", "latitude", "longitude", "address", "battery", "volume"
-    )
 
 api_router = DefaultRouter()
 api_router.register(prefix=r"seizures", viewset=APISeizuresViewSet)
