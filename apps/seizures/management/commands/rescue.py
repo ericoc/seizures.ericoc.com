026261903402a9f76f7435b031d273a6e81cbcb4
Eric O'Callaghan <eric@ericoc.com>
2025-05-11 14:56:55 -0400

    rescue email

--- apps/seizures/management/commands/rescue.py
+++ apps/seizures/management/commands/rescue.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python3
+from datetime import datetime, timedelta
+from django.conf import settings
 from django.contrib.humanize.templatetags.humanize import intcomma, naturaltime
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
-from django.utils.timezone import localtime, timedelta
+from django.utils import timezone
 from django.utils.translation import ngettext
+from pathlib import Path
 
 from ...models import Seizure
 
 
-# Date/time format.
-DT_FMT = "%A, %B %d, %Y @ %I:%M:%S %p %Z %z"
-
-
 class Command(BaseCommand):
     """
-    List seizures if there are more than X in the past Y hours.
+    List, and optionally e-mail, seizures if >=X in past Y hour(s).
     """
-    help = "List seizures if there are more than X in the past Y hours."
+    help = "List, and optionally e-mail, seizures if >=X in past Y hour(s)."
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -27,34 +27,86 @@ class Command(BaseCommand):
         parser.add_argument(
             "--threshold", "-threshold", "-t", "--t",
             default=15,
-            help="Threshold of # of seizures to search for. (Default: 15)",
+            help="Threshold of # of seizure(s) to search for. (Default: 15)",
             type=int,
         )
 
     def handle(self, *args, **options):
-        """Count recent seizures."""
+
+        # Human-readable date format.
+        dt_fmt = "%A, %B %d, %Y @ %I:%M:%S %p %Z %z"
+
+        # Get the number of hours to search back from now for seizures.
         hours = options.get("hours")
-        since = localtime() - timedelta(hours=hours)
-        threshold = options.get("threshold")
+
+        # Create a datetime of the time however many hours ago.
+        since = timezone.localtime() - timedelta(hours=hours)
+
+        # Find and count seizures going back to time requested.
         seizures_objs = Seizure.objects.filter(pk__gte=since)
         num_seizures = seizures_objs.count()
 
-        if num_seizures >= threshold:
-            message = "Found %s %s since %s on %s.\n" % (
-                intcomma(num_seizures),
-                ngettext(
+        # List the total number seizures found since when.
+        message = "Found %s %s, since %s (on %s).\n" % (
+            intcomma(num_seizures),
+            ngettext(
                 singular="seizure",
-                    plural="seizures",
-                    number=num_seizures
-                ),
-                naturaltime(since),
-                since.strftime(DT_FMT),
-            )
+                plural="seizures",
+                number=num_seizures
+            ),
+            naturaltime(since),
+            since.strftime(dt_fmt),
+        )
+
+        # Proceed if number of seizures found is >= the threshold given.
+        threshold = options.get("threshold")
+        if num_seizures >= threshold:
+
+            # Count, and list, the details of when/where/etc. each seizure.
+            message += "\n"
             for (count, seizure) in enumerate(seizures_objs.all(), start=1):
                 message += "%i. %s / %s / %s\n" % (
                     count,
+                    timezone.localtime(value=seizure.pk).strftime(dt_fmt),
                     seizure.device_type,
-                    localtime(value=seizure.pk).strftime(DT_FMT),
                     str(seizure.address).replace("\n", ", "),
                 )
-            self.stdout.write(self.style.SUCCESS(message))
+
+            # Specify file to record e-mail time, to avoid repeated messages.
+            do_mail = False
+            last_mail = Path(Path.home(), "last_mail")
+            try:
+
+                # Get modification time (mtime) of last e-mail file.
+                mailed = timezone.make_aware(
+                    datetime.fromtimestamp(last_mail.stat().st_mtime)
+                )
+
+                # Skip e-mailing if an e-mail was sent within threshold.
+                if mailed >= since:
+                    message += "\nSKIPPED threshold e-mail.\n"
+                    message += f"E-mail was sent {mailed.strftime(dt_fmt)}."
+                    message += f" (>= {since.strftime(dt_fmt)}).\n"
+                else:
+                    do_mail = True
+
+            # Prepare to send e-mail if the file did not exist.
+            except FileNotFoundError:
+                do_mail = True
+
+            # Send the e-mail and say so, if it is okay to do so.
+            if do_mail is True:
+                if send_mail(
+                    subject=f"{settings.WEBSITE_TITLE}: Rescue",
+                    message=message,
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=(settings.ADMINS[0][1],),
+                ):
+                    message += "\nSENT threshold e-mail.\n"
+
+                    # Create file to record time when the e-mail was sent.
+                    last_mail.touch()
+
+
+        # Finally, show the (on-screen) message!
+        self.stdout.write(self.style.SUCCESS(message))
