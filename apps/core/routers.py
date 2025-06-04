260f3ad8cb03039bbbeffd38fa030cfbe0fbb051
Eric O'Callaghan <eric@ericoc.com>
2025-06-04 13:58:25 -0400

    drop snowflake

--- apps/core/routers.py
+++ apps/core/routers.py
@@ -1,14 +1,14 @@
 class SeizureRouter:
-    """Database router routes seizures reads and writes to Snowflake."""
+    """Database router routes seizures reads and writes to SQLite."""
 
     def db_for_read(self, model, **hints):
         if model._meta.required_db_vendor:
-            if model._meta.required_db_vendor == "snowflake":
+            if model._meta.required_db_vendor == "sqlite":
                 return model._meta.app_label
         return "default"
 
     def db_for_write(self, model, **hints):
         if model._meta.required_db_vendor:
-            if model._meta.required_db_vendor == "snowflake":
+            if model._meta.required_db_vendor == "sqlite":
                 return model._meta.app_label
         return "default"
--- settings.example.py
+++ settings.example.py
@@ -161,23 +161,6 @@ DATABASES = {
         'HOST': 'localhost',
     },
     'seizures': {
-        'ENGINE': 'django_snowflake',
-        'NAME': 'seizures',
-        'SCHEMA': 'seizures',
-        'WAREHOUSE': 'seizures',
-        'USER': 'seizures',
-        # 'PASSWORD': 'EXAMPLE', ## Private key or MFA.
-        'ACCOUNT': 'example-id',
-        'TIMEZONE': TIME_ZONE,
-        # Include 'OPTIONS' if you need to specify any other
-        # snowflake.connector.connect() parameters.
-        # https://docs.snowflake.com/en/user-guide/python-connector-api.html#connect
-        'OPTIONS': {
-            'TIMEZONE': TIME_ZONE,
-            'private_key_file': Path(Path.home(), ".snowsql/rsa_key.p8"),
-        },
-    },
-    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': Path(Path(BASE_DIR), Path('seizures.sqlite3')),
         'SCHEMA': 'seizures',
