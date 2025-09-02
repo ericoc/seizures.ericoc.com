aa5499610cb01f537b96a9b0fca9751e80c46bc8
Eric O'Callaghan <eric@ericoc.com>
2025-09-01 20:07:04 -0400

    clean up dow sql logic, rm broken stuff

--- apps/seizures/management/commands/__init__.py
+++ apps/seizures/management/commands/__init__.py
@@ -1,5 +1,3 @@
 from .dow import Command as DOWCommand
-from .dow2 import Command as DOWCommand
 from .recent import Command as RecentCommand
-from .timespan import Command as TimespanCommand
 from .tosqlite import Command as ToSQLiteCommand
--- apps/seizures/management/commands/dow.py
+++ apps/seizures/management/commands/dow.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python3
 from calendar import day_name
 from sys import exit
-from django.contrib.humanize.templatetags.humanize import intcomma
 from django.core.management.base import BaseCommand
+from django.db.models.aggregates import Count
+from django.db.models.expressions import F
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 from ...models import Seizure
 
 
 class Command(BaseCommand):
     """
-    Count number of seizures by day of the week.
+    Count number of seizures by day of the week, over all time.
     """
-    help = "Count number of seizures by day of the week."
+    help = "Count number of seizures by day of the week, over all time."
 
     def handle(self, *args, **options):
-        """Loop through each day of the week, querying seizures for each."""
-        for dow_num in range(1, 8):
-            dow_name = day_name[dow_num -1]
-            dow_count =  Seizure.objects.filter(
-                timestamp__week_day=dow_num
-            ).count()
+
+        # Query seizures and count by day of the week.
+        seizures_per_dow = Seizure.objects.values(
+                weekday=F("timestamp__week_day")
+            ).annotate(
+                seizures=Count("timestamp__week_day")
+            ).order_by("weekday").all()
+
+        # Loop through each day of the week to display number of seizures.
+        for item in seizures_per_dow:
+            dow_num = item["weekday"]
+            dow_name = day_name[dow_num - 1]
+            seizure_count = item["seizures"]
             self.stdout.write(
                 self.style.SUCCESS(
-                    f"{dow_num}. {dow_name}\t{intcomma(dow_count)}"
+                    f"{item['weekday']}. {dow_name}\t{intcomma(seizure_count)}"
                 )
             )
+
         exit(0)
