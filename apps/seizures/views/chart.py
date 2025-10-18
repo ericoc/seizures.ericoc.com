52b542c038e6f13c1c69995ff5f5e5c4a41bc1af
Eric O'Callaghan <eric@ericoc.com>
2025-10-18 18:42:23 -0400

    remove reference to solved issue #3

--- apps/seizures/views/chart.py
+++ apps/seizures/views/chart.py
@@ -5,6 +5,3 @@ class SeizuresChartView(SeizuresBaseView):
     """Highcharts view."""
     days = 365
     template_name = "chart.html"
-
-    # TODO: Update SeizuresChartView query to chart days with zero (0) seizures:
-    # https://github.com/ericoc/seizures.ericoc.com/issues/3
