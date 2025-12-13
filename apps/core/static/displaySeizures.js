bce7e451b3b397c5a6449be51d0a0428d849bf14
Eric O'Callaghan <eric@ericoc.com>
2025-12-12 21:32:35 -0500

    static fixup

---
+++ apps/core/static/displaySeizures.js
@@ -0,0 +1,47 @@
+/* Loop through each seizure, displaying them. */
+async function displaySeizures(seizureData) {
+
+    // Parse JSON containing seizure data.
+    // Start seizure navigation list items.
+    const seizures = Object.values(JSON.parse(JSON.parse(seizureData)));
+    const seizureCount = seizures.length;
+
+    // Loop through seizures, displaying each.
+    for (const seizure of seizures) {
+        await displaySeizure(seizure);
+    }
+
+    // Append the list of seizure links to the navigation card.
+    seizuresCard.appendChild(seizureList);
+
+    // Add a card footer of the seizure count.
+    const cardFooter = document.createElement("h5");
+    cardFooter.classList.add("card-footer");
+    cardFooter.classList.add("text-secondary");
+    cardFooter.classList.add("text-center");
+    let countText = `${seizureCount.toLocaleString("en-US")} seizure`;
+    if (seizureCount > 1) { countText += "s"; }
+    document.title += ` ${countText}`;
+    cardFooter.appendChild(document.createTextNode(countText));
+    cardFooter.title = countText;
+    seizuresCard.appendChild(cardFooter);
+
+    // Set map bounds using minimum and maximum seizure coordinates.
+    map.fitBounds([
+        [Math.min(...latitudes), Math.min(...longitudes)],
+        [Math.max(...latitudes), Math.max(...longitudes)]
+    ]);
+
+    // Open popup for marker if referenced by URL anchor.
+    const anchor = String(window.location.hash).split('#')[1];
+    if (anchor) {
+        const marker = markers[anchor];
+        if (marker) { marker.openPopup(); }
+    }
+
+    // Open popup if single marker.
+    if (seizureCount === 1) {
+        markers[Object.keys(markers)[0]].openPopup();
+    }
+
+}
