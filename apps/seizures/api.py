d633862fb756bcd383a05cb817be8a9b01a539b4
Eric O'Callaghan <eric@ericoc.com>
2025-12-30 21:38:00 -0500

    update descriptions/titles

--- apps/seizures/api.py
+++ apps/seizures/api.py
@@ -1,6 +1,6 @@
 from django.utils.timezone import localtime, timedelta
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import BasePermission, IsAdminUser, SAFE_METHODS
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.routers import DefaultRouter
 from rest_framework.serializers import ModelSerializer
 from rest_framework.viewsets import ModelViewSet
@@ -39,46 +39,33 @@ class BaseAPISeizuresViewSet(ModelViewSet):
 
 """Admin ViewSet."""
 class APISeizuresViewSet(BaseAPISeizuresViewSet):
-    """Django-Rest-Framework (DRF) ModelViewSet API endpoint of seizures."""
+    """All fields."""
     filterset_fields = fields = ordering_fields = "__all__"
     permission_classes = (IsAdminUser,)
     serializer_class = SeizureSerializer
 
-    def get_queryset(self):
-        """Optionally filter queryset by device type (?device_type=iPhone)."""
-        qs = super().get_queryset()
-
-        device_type = self.request.query_params.get("device_type")
-        if device_type is not None:
-            qs = qs.filter(device_type__exact=device_type)
-
-        return qs
+    def get_view_name(self):
+        """Update Django-Rest-Framework (DRF) admin view name title."""
+        return "Administrative API"
 
 """Public ViewSet (ReadOnly)."""
-class ReadOnly(BasePermission):
-    """Create read-only permissions for class PublicAPISeizuresViewSet."""
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-
 class PublicAPISeizuresViewSet(BaseAPISeizuresViewSet):
-    """Django-Rest-Framework (DRF) ModelViewSet public API endpoint of timestamps
-        of seizures older than thirty (30) days."""
+    """Timestamps older than thirty (30) days."""
     filterset_fields = fields = ordering_fields = ("timestamp",)
     model = Seizure
-    permission_classes = (ReadOnly,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = model.objects.filter(
         timestamp__lte=localtime()-timedelta(days=30)
     )
     serializer_class = PublicSeizureSerializer
 
-    def has_permission(self, request, view):
-        return request.method in SAFE_METHODS
-
-    def has_object_permission(self, request, view, obj):
-        return request.method in SAFE_METHODS
+    def get_view_name(self):
+        """Update Django-Rest-Framework (DRF) public view name title."""
+        return "Public Timestamps"
 
 
 """Create API router, and endpoints."""
 api_router = DefaultRouter()
+api_router.get_api_root_view().cls.__name__ = "Seizures"
 api_router.register(prefix=r"seizures", viewset=APISeizuresViewSet, basename="seizures")
 api_router.register(prefix=r"public", viewset=PublicAPISeizuresViewSet, basename="public")
